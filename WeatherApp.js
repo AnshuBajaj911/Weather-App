9469c1e349f174f60121b0877167ea119cde1bf8
Anshu Bajaj <64094461+AnshuBajaj911@users.noreply.github.com>
2020-09-20 15:08:59 +0530

    Update WeatherApp.js

--- WeatherApp.js
+++ WeatherApp.js
@@ -39,4 +39,44 @@ var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
                 )
             }
         ) }
+
+function f22(){
+    var coo =document.getElementById("d").value;
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+     
+         var api_key='&appid=106a2e096818d8962a6e43b32eeeb84d&units=metric'
+         var url = api+coo+api_key;
+     
+        fetch(url).then(
+            res=>{
+                res.json().then(
+                    data=>{
+                        
+                     console.log(data);
+                    
+                         var temp="";
+
+                        
+                           
+                             temp+="<tr class='table-success'>";
+                             temp+="<td>"+data.name+"</td>";
+                              temp+="<td>"+data.sys.country+"</td>";
+                             temp+="<td>"+data.weather[0].description+"</td>";
+                             temp+="<td>"+data.main.temp+"</td>";
+                            temp+="<td>"+data.main.temp_max+"</td>";
+                             temp+="<td>"+data.main.temp_min+"</td>";
+                             
+                           temp+="</tr>"
+                           
+
+                        
+
+                        
+                         document.getElementById("data").innerHTML += temp;
+                     
+                    
+                    }
+                )
+            }
+        ) }
    
